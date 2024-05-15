a456961c28f097d7a59518e1b8c4a03a84182b89
odasolhaug <odasolh@viken.no>
2024-05-15 21:03:13 +0200

    h
    h

--- basket.js
+++ basket.js
@@ -9,6 +9,7 @@ const poeng2_output = document.getElementById("poeng2")
 
 let poeng1 = 0
 let poeng2 = 0
+let vinner = 0
 
 const ramme = document.getElementById("ramme")
 
@@ -44,10 +45,24 @@ const kurv_hoyre = {
 }
 
 function startSpill() {
-    var overlay = document.getElementById("overlay");
-    overlay.style.display = "none";
-  }
+    let overlay = document.getElementById("overlay")
+    overlay.style.display = "none"
+}
+function vinner() {
+    let overlay_end = document.getElementById("overlay_end")
+    overlay_end.style.display = "block"
+}
 
+function sjekkPoengsum() {
+    if (poeng1 === 10) {
+       vinner = 1
+        vinner()
+    }
+    else if (poeng2 === 10) {
+        vinner = 2
+        vinner()
+    }
+}
 
 function flytt(ball) {
     ball.y_velocity += ball.y_gravity
@@ -71,30 +86,32 @@ function flytt(ball) {
 
 
 function kollisjon(ball, kurv, ballNum) {
-    if (ball.x + ball.diameter > kurv.x && ball.x < kurv.x + kurv_width && ball.y + ball.diameter > kurv.y && ball.y < kurv.y + kurv_height && ball.y < ramme_height - ball.diameter - 30 && ball.y_velocity > 0)  {
+    if (ball.x + ball.diameter > kurv.x && ball.x < kurv.x + kurv_width && ball.y + ball.diameter > kurv.y && ball.y < kurv.y + kurv_height && ball.y < ramme_height - ball.diameter - 30 && ball.y_velocity > 0) {
         ball.x_velocity = -ball.x_velocity
         ball.y_velocity = -ball.y_velocity
     }
-    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
 
-    if (ball.y > ramme_height - ball.diameter - 30 && ball.x + ball.diameter > kurv.x -55 && ball.x < kurv.x + kurv_width-55 && ballNum === 1 && !ball.harTruffetKurv) {
+    if (ball.y > ramme_height - ball.diameter - 30 && ball.x + ball.diameter > kurv.x - 55 && ball.x < kurv.x + kurv_width - 55 && ballNum === 1 && !ball.harTruffetKurv) {
         ball.y_gravity = -2
         poeng1 += 1
-        poeng1_output.innerHTML = "SCORE: " +poeng1
+        poeng1_output.innerHTML = "SCORE: " + poeng1
         ball.harTruffetKurv = true
 
+        sjekkPoengsum()
         ramme.style.backgroundColor = "#" + randomColor
         //ball.y_velocity = -ball.y_velocity
         //&& ball.y > kurv.y + kurv_height
     }
-    else if (ball.y > ramme_height - ball.diameter - 30 && ball.x + ball.diameter > kurv.x + 55 && ball.x < kurv.x + kurv_width +55 && ballNum === 2 && !ball.harTruffetKurv) {
+    else if (ball.y > ramme_height - ball.diameter - 30 && ball.x + ball.diameter > kurv.x + 55 && ball.x < kurv.x + kurv_width + 55 && ballNum === 2 && !ball.harTruffetKurv) {
         ball.y_gravity = -2
         //if (ball.y_gravity === -2 && ball.y === 200) {
-            poeng2 += 1 
-            poeng2_output.innerHTML = "SCORE: " + poeng2
-            ball.harTruffetKurv = true
+        poeng2 += 1
+        poeng2_output.innerHTML = "SCORE: " + poeng2
+        ball.harTruffetKurv = true
 
-            ramme.style.backgroundColor = "#" + randomColor
+        sjekkPoengsum()
+        ramme.style.backgroundColor = "#" + randomColor
         //}
         //poeng2 += 1 
         //poeng2_output.innerHTML = "Score ball 2: " + poeng2
@@ -102,7 +119,7 @@ function kollisjon(ball, kurv, ballNum) {
 }
 
 function kollisjon_mellom_ballene() {
-    if (Math.abs(ball1.x - ball2.x) < ball1.diameter/2 && Math.abs(ball1.y - ball2.y) < ball1.diameter/2 && antalltegn > 100) {
+    if (Math.abs(ball1.x - ball2.x) < ball1.diameter / 2 && Math.abs(ball1.y - ball2.y) < ball1.diameter / 2 && antalltegn > 100) {
         ball1.x_velocity = -ball1.x_velocity;
         ball2.x_velocity = -ball2.x_velocity;
         ball1.y_velocity = -ball1.y_velocity;
